1a3fde5c0e139e79ba3a132769deaaed69b40b9d
gujarats <gujarat.santana@gmail.com>
2017-12-05 16:48:01 +0700

    Publishing to gh-pages

--- js/theme.js
+++ js/theme.js
@@ -1,18 +1,18 @@
 (function() {
     var elements = document.querySelectorAll('[data-open]');
+    var className = 'open';
     for (var i=0; i<elements.length; i++) {
-        initOpen(elements[i]);
+        // skip opening items tags
+        if (i==1){
+            initClose(elements[i]);
+        }else{
+            initOpen(elements[i]);
+        }
+        
     }
 
     function initOpen(element) {
-        var className = 'open';
         var target = document.getElementById(element.getAttribute('data-open'));
-        var ulChildrenMap = function(fn) {
-            var lists = target.getElementsByTagName('ul');
-            for (var i=0; i<lists.length; i++) {
-                fn(lists[i]);
-            }
-        };
         if (!target) return;
 
         // dirty hack for small screen ...
@@ -20,6 +20,31 @@
         if (firstChildUl && window.getComputedStyle(firstChildUl).display === 'none') {
             target.classList.remove(className);
         }
+
+        addEventListener(className,element,target)
+    }
+
+    function initClose(element){
+        var target = document.getElementById(element.getAttribute('data-open'));
+
+        if (!target) return;
+        
+        // removing all child elements 
+        target.classList.remove(className);
+        ulChildrenMap(function(ul) { ul.style.display = 'none'; });
+
+        addEventListener(className,element,target)
+    }
+
+
+    function addMenuEventListener(className,element,target){
+
+        var ulChildrenMap = function(fn) {
+            var lists = target.getElementsByTagName('ul');
+            for (var i=0; i<lists.length; i++) {
+                fn(lists[i]);
+            }
+        };
 
         element.addEventListener('click', function() {
             var list = target.getElementsByTagName('ul')[0];
