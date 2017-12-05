4355ff83c1e22028cb16d65fe3c5f7ef9d78310c
gujarats <gujarat.santana@gmail.com>
2017-12-05 11:27:08 +0700

    publish script

---
+++ publish-gh-pages.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# remove previous publication
+rm -rf public
+mkdir public
+
+# clone gh-pages branch from the local repo into a repo located within "public"
+git clone .git --branch gh-pages public
+  
+# generate
+hugo
+  
+# commit the changes in the clone and push them back to the local gh-pages branch    
+cd public && git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages
+
+#publish
+git checkout gh-pages && cd public/ && mv * .[^.]* .. && git push origin gh-pages
