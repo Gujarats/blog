a8b30ef36ed4c46aa20c8f126f030d8fbecb9cc2
gujarats <gujarat.santana@gmail.com>
2017-12-05 13:24:12 +0700

    publish script

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # remove previous publication
 rm -rf public
@@ -13,5 +13,17 @@ hugo
 # commit the changes in the clone and push them back to the local gh-pages branch    
 cd public && git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages
 
+#change branch
+cd ..
+git checkout gh-pages
+
+#removing all files except
+shopt -s extglob
+rm -rf -- !(public|git|publish.sh)
+
+#moving all files to root
+cd public/ && mv * .[^.]* .. 
+cd ..
+
 #publish
-git checkout gh-pages && cd public/ && mv * .[^.]* .. && git push origin gh-pages
+git push origin gh-pages
